f9b117538229e986bf190882f58b1d3c4195bf37
Mateus Ribeiro <MatZeraH64@hotmail.com>
2026-05-01 20:55:13 -0300

    Classe que representa um lote de medicamentos

---
+++ Domain/Lote.py
@@ -0,0 +1,32 @@
+import datetime
+import decimal
+
+from sqlalchemy import String, Integer, Boolean, DateTime, func, ForeignKey, DECIMAL
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+from Domain.Base import Base
+from Domain.Enums.StatusLote import StatusLote
+
+
+class Lote(Base):
+    __tablename__ = 'lote'
+    
+    id : Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    codigo_lote : Mapped[str] = mapped_column(String(50), nullable=False, unique=True)
+    
+    medicamento_id : Mapped[int] = mapped_column(Integer, ForeignKey('medicamento.id'))
+    fornecedor_id : Mapped[int] = mapped_column(Integer, ForeignKey('fornecedor.id'))
+    
+    data_fabricacao : Mapped[datetime.datetime] = mapped_column(DateTime)
+    data_validade : Mapped[datetime.datetime] = mapped_column(DateTime, index=True, nullable=False)
+    
+    quantidade_inicial : Mapped[int] = mapped_column(Integer, default=0)
+    quantidade_vendida : Mapped[int] = mapped_column(Integer, default=0)
+    quantidade_disponivel : Mapped[int] = mapped_column(Integer, default=0)
+    
+    preco_unitario : Mapped[decimal.Decimal] = mapped_column(DECIMAL, default=0)
+    status : Mapped[StatusLote] = mapped_column(StatusLote)
+    data_recebimento : Mapped[datetime.datetime] = mapped_column(DateTime)
+    
+    created_at: Mapped[datetime.datetime] = mapped_column(DateTime, default=func.now())
+    updated_at: Mapped[datetime.datetime] = mapped_column(DateTime, default=func.now(), onupdate=func.now())
