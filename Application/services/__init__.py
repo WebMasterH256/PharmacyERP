2707cf7f957d5412f9c9f9bd1386d06e834433a0
MatZe <MatZeraH64@hotmail.com>
2026-05-14 00:33:44 -0300

    Entrada da camada dos services

---
+++ Application/services/__init__.py
@@ -0,0 +1,13 @@
+from .estoque_service import EstoqueService
+from .alerta_service import AlertaService
+from .compra_service import CompraService
+from .medicamento_service import MedicamentoService
+from .relatorio_service import RelatorioService
+
+__all__ = [
+	"EstoqueService",
+	"AlertaService",
+	"CompraService",
+	"MedicamentoService",
+	"RelatorioService",
+]
